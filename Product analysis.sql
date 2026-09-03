8659f41a253318e4eb2bab646a9f1849c19ffca4
Le Hoang Phuong Trang <trangphuong.hoangle43@gmail.com>
2026-09-03 22:35:15 +0700

    Update Product analysis.sql

--- Product analysis.sql
+++ Product analysis.sql
@@ -1,3 +1,4 @@
+/* Top 10 products by revenue over time */
 select ProductCategory
   ,ProductName
   ,sum(SalesAmount) as Revenue
@@ -9,3 +10,22 @@ on dpd.ProductSubcategoryKey = dcat.ProductSubcategoryKey
 group by 1,2
 order by 3 desc
 limit 10
+
+ /* Revenue share by category */ 
+with raw as
+(select extract(year from OrderDate) as Year
+  , ProductCategory
+  , sum(SalesAmount) as Revenue
+from jda-k1.practice_data_pipeline.trfsales as fact
+left join jda-k1.practice_data_pipeline.trdproduct as dpd
+on fact.ProductKey = dpd.ProductKey
+left join jda-k1.practice_data_pipeline.trdproductsubcategory as dcat
+on dpd.ProductSubcategoryKey = dcat.ProductSubcategoryKey
+group by 1, 2
+order by 1 asc
+)
+select Year 
+  , ProductCategory
+  , Revenue
+  , round(Revenue * 100 / sum(Revenue) over(partition by Year), 2) as Rev_share
+from raw
