f0f50b0cf444016de0e037c48a9a463ece3896d1
Le Hoang Phuong Trang <trangphuong.hoangle43@gmail.com>
2026-09-09 17:37:10 +0700

    Update Product analysis.sql

--- Product analysis.sql
+++ Product analysis.sql
@@ -12,17 +12,17 @@ order by 3 desc
 limit 10
 
 /* Result
-Category	ProductName	Revenue
+Category	ProductName	        Revenue
 Bikes	Mountain-200 Black, 46	1373454
 Bikes	Mountain-200 Black, 42	1363128
 Bikes	Mountain-200 Silver, 38	1339394
 Bikes	Mountain-200 Silver, 46	1301029
 Bikes	Mountain-200 Black, 38	1294854
 Bikes	Mountain-200 Silver, 42	1257368
-Bikes	Road-150 Red, 48	1205786
-Bikes	Road-150 Red, 62	1202208
-Bikes	Road-150 Red, 52	1080556
-Bikes	Road-150 Red, 56	1055510
+Bikes	Road-150 Red, 48	      1205786
+Bikes	Road-150 Red, 62	      1202208
+Bikes	Road-150 Red, 52	      1080556
+Bikes	Road-150 Red, 56	      1055510
 */
 
  /* Revenue share by category */ 
