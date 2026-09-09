83d52af6dc07f0737d1f85ddcec3a0c5dcdf5d95
Le Hoang Phuong Trang <trangphuong.hoangle43@gmail.com>
2026-09-09 17:31:56 +0700

    Update Product analysis.sql

--- Product analysis.sql
+++ Product analysis.sql
@@ -22,19 +22,19 @@ on fact.ProductKey = dpd.ProductKey
 left join jda-k1.practice_data_pipeline.trdproductsubcategory as dcat
 on dpd.ProductSubcategoryKey = dcat.ProductSubcategoryKey
 group by 1, 2
-order by 1 asc
 )
 select Year 
   , ProductCategory
   , Revenue
   , round(Revenue * 100 / sum(Revenue) over(partition by Year), 2) as Rev_share
 from raw
+order by 1 asc, 3 desc
 
  /* Result 
 Year	ProductCategory	Revenue	Rev_share
-2010	Bikes	      43419	100
-2011	Bikes	7075088	100
-2012	Bikes	5839443	99.95
+2010	Bikes	        43419	100
+2011	Bikes	        7075088	100
+2012	Bikes	        5839443	99.95
 2012	Accessories	2146	0.04
 2012	Clothing	642	0.01
 2013	Bikes	15358322	93.94
