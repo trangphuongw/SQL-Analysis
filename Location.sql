b095a4b0e2d1b3411d996f160498aaa29d41833a
Le Hoang Phuong Trang <trangphuong.hoangle43@gmail.com>
2026-09-09 15:52:36 +0700

    Update Location.sql

--- Location.sql
+++ Location.sql
@@ -1,1 +1,15 @@
-
+*/ Order by countries based on revenue/ rev share each year/*
+with loc as 
+(select extract(year from OrderDate) as Year
+  , SalesTerritoryCountry as Location
+  , sum(SalesAmount) as Revenue
+from jda-k1.practice_data_pipeline.trfsales as fact
+left join jda-k1.practice_data_pipeline.trdsalesterritory as reg
+on fact.SalesTerritoryKey = reg.SalesTerritoryKey
+group by 1,2
+order by 1 asc
+)
+select *
+  , round (Revenue * 100 / sum(Revenue) over(partition by Year), 2) as Rev_share
+from loc
+order by 1 asc, 4 desc
