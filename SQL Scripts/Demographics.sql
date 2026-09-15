2072e663b91b70ab98ce215603c5cb46c2e32648
Le Hoang Phuong Trang <trangphuong.hoangle43@gmail.com>
2026-09-15 17:33:48 +0700

    Create Demographics.sql

---
+++ SQL Scripts/Demographics.sql
@@ -0,0 +1,14 @@
+/* Gender */
+select Gender
+  , sum(SalesAmount) as Revenue
+  , round(sum(SalesAmount) / sum(sum(SalesAmount)) over () , 4) as Rev_share
+from jda-k1.practice_data_pipeline.trfsales as fact
+left join jda-k1.practice_data_pipeline.trdcustomer as cus
+on fact.CustomerKey = cus.CustomerKey
+group by Gender
+
+/* Result 
+Gender	Revenue	       Rev_share
+F	      14,812,104	   5.046
+M	      14,543,606	   4.954
+*/
