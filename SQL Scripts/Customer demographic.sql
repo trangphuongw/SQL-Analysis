408d0ce62e1d7e9f65571a07ad6b24fff077907e
Le Hoang Phuong Trang <trangphuong.hoangle43@gmail.com>
2026-09-15 14:31:33 +0700

    Update Customer demographic.sql

--- SQL Scripts/Customer demographic.sql
+++ SQL Scripts/Customer demographic.sql
@@ -10,7 +10,16 @@ group by 1,2
 select Year
   , count(distinct CustomerKey) as Total_buyers
   , count(case when orders >= 2 then CustomerKey end) as Return_buyers
-  , round(count(case when orders >= 2 then CustomerKey end) * 100 / count(distinct CustomerKey)) as Return_rate
+  , round(count(case when orders >= 2 then CustomerKey end) * 100 / count(distinct CustomerKey), 2) as Return_rate
 from customer
 group by 1
 order by 1 asc
+
+/* Results
+Year	Total_buyers	Return_buyers	Return_rate
+2010	14	          0	0
+2011	2216	        0	0
+2012	3255	        14	0.43
+2013	17429	        2665	15.29
+2014	834	30	      3.6
+*/
